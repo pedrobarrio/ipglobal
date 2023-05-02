39213295f73ad4781959dfc8d4d37c0b3d00ab1a
pedro.barrio <pedro.barrio@netexcloud.net>
2023-05-02 14:02:58 +0200

    add phpunit library

--- src/App/Domain/Author/Author.php
+++ src/App/Domain/Author/Author.php
@@ -1,11 +1,13 @@
 <?php
 declare(strict_types=1);
-namespace App\App\Domain\Author;
 
+namespace App\App\Domain\Author;
 
 use App\Shared\Aggregate\AggregateRoot;
 
 class Author extends AggregateRoot
 {
-
+    public function __construct(private readonly AuthorId $id, private readonly string $name)
+    {
+    }
 }
--- src/App/Domain/Post/Post.php
+++ src/App/Domain/Post/Post.php
@@ -1,10 +1,17 @@
 <?php
 declare(strict_types=1);
+
 namespace App\App\Domain\Post;
 
 use App\Shared\Aggregate\AggregateRoot;
 
 class Post extends AggregateRoot
 {
-
+    public function __construct(
+        private readonly PostId $id,
+        private readonly string $name,
+        private readonly string $content
+    )
+    {
+    }
 }
