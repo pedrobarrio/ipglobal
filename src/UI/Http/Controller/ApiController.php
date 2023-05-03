107253c1dbe16436269f80e1ebdbb4329cdb1938
pedro.barrio <pedro.barrio@netexcloud.net>
2023-05-03 11:11:09 +0200

    add endpoints

--- src/UI/Http/Controller/ApiController.php
+++ src/UI/Http/Controller/ApiController.php
@@ -1,5 +1,5 @@
 <?php
-
+declare(strict_types=1);
 namespace App\UI\Http\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -8,8 +8,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ApiController extends AbstractController
 {
-    #[Route('/api', name: 'app_api')]
-    public function index(): JsonResponse
+    #[Route('/api', name: 'app_api', methods: ['GET'])]
+    public function getPost(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new API controller!',
+            'path' => 'src/Controller/ApiController.php',
+        ]);
+    }
+
+    #[Route('/api', name: 'app_api', methods: ['POST'])]
+    public function publicPost(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new API controller!',
--- src/UI/Http/Controller/BlogController.php
+++ src/UI/Http/Controller/BlogController.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace App\UI\Http\Controller;
 
@@ -8,8 +9,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
+    #[Route('/blog', name: 'app_blog', methods: ['GET'])]
     public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new BLOG controller!',
+            'path' => 'src/Controller/BlogController.php',
+        ]);
+    }
+
+    #[Route('/blog/{id}', name: 'app_blog_post', methods: ['POST'])]
+    public function getPost(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new BLOG controller!',
